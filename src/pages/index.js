209fb056dd7bab3e37eb253bc8c6428f92ce950c
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-11-09 23:37:16 +0100

    fix: redirects on not found

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,24 @@
 import { createElement } from 'react';
 import { connect } from 'react-redux';
-import { STORIES, STORIES_SLUG } from 'router';
+import { redirect, NOT_FOUND } from 'redux-first-router';
+
+import { HOME, STORIES, STORIES_SLUG } from 'router';
 import Stories from './stories/stories';
 
 const pages = {
   [STORIES]: Stories,
-  [STORIES_SLUG]: Stories
+  [STORIES_SLUG]: Stories,
+  [NOT_FOUND]: Stories
 };
 
+const mapDispatchToProps = dispatch => ({
+  onNotFound: () => dispatch(redirect({ type: HOME }))
+});
 const mapStateToProps = ({ location }) => ({ page: location.type });
 
-const Pages = ({ page }) => createElement(pages[page]);
+const Pages = ({ page, onNotFound }) => {
+  if (page === NOT_FOUND) onNotFound();
+  return createElement(pages[page]);
+};
 
-export default connect(mapStateToProps)(Pages);
+export default connect(mapStateToProps, mapDispatchToProps)(Pages);
