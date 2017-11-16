5d39127d6e0375d7d8f6c29901936daa267a39b6
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-11-16 15:56:00 +0100

    includes main gallery slider

--- src/components/image-gallery/image-gallery.component.jsx
+++ src/components/image-gallery/image-gallery.component.jsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import ThumbnailGallery from './thumbnail-gallery.component';
+import Slider from 'react-slick';
+
+const settings = {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: false
+};
 
 function ImageGallery(props) {
   return (
-    <section id="mainGallery" className="main-gallery">
-      <div className="main-gallery-container">
-        <div className="main-gallery-holder">
-          <div className="switchers-area">
-            <p className="switcher-close">
-              <button className="gallery-btn"
-                      title="close">
-                <i className="icon-close-bold" />
-              </button>
-            </p>
-          </div>
-          <section className="main-gallery-box">
-            <figure className="gallery-box-item">
-              <img
-                className="slide-banner"
-                src="images/gallery/image.png"
-                alt="Name of the Photo" />
-              <figcaption className="slide-content">
-                <h5>Name of the Photo</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-              </figcaption>
-            </figure>
-          </section>
+    <section className="main-gallery-container">
+      <div className="main-gallery-holder">
+        <div className="main-gallery-box">
+          <Slider {...settings}>
+            <div className="gallery-box-item">
+              <div className="gallery-slide" style={{ backgroundImage: 'url("/images/article/img4.png")' }} />
+            </div>
+          </Slider>
+        </div>
+        <div className="slide-content">
+          <h5>Name of the Photo</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        </div>
+      </div>
+      <div className="switchers-area">
+        <div className="preview-container">
+          <p className="switcher-close">
+            <button className="gallery-btn"
+                    title="close">
+              <i className="icon-close-bold" />
+            </button>
+            <ThumbnailGallery/>
+          </p>
         </div>
       </div>
     </section>
