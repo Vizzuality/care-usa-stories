9e4f9645dde6d04f07bb0f5219837f1c7c2a8d8e
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-11-20 17:44:57 +0100

    feat: parse markdown body text

---
+++ src/components/text-content.component.jsx
@@ -0,0 +1,29 @@
+import React, { Children }  from 'react';
+
+function TextContent({ children }) {
+  const content = Children.map(children, (child, i) => {
+    if (typeof child === 'string') {
+      const [textContent, ...bulletPoints] = child.split('- ');
+      return [
+        <div className="std" key={`main-text-content${i}`}>
+          <p>{textContent}</p>
+        </div>,
+        <div className="std list" key={`list-text-content${i}`}>
+          <ul>
+            {
+              bulletPoints.map(bulletPoint => (
+                <li key={bulletPoint}>
+                  {bulletPoint}
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      ]
+    }
+    return child;
+  });
+  return content;
+}
+
+export default TextContent;
